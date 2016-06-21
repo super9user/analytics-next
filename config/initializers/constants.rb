da564418a09f064ce19f907aa4dc925f7c25143d
Tanmay Patil <tanmay.ashok.patil@gmail.com>
2016-06-21 00:16:25 -0700

    Moved stuff from db_seed to constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,6 @@
 HOSTING_SERVICES_SUPPORTED = ["github"].freeze
-
 ERROR_SEVERITY = {high: 'high', medium: 'medium', low: 'low'}.freeze
+
+ADMINISTRATOR = {first_name: "Tanmay", last_name: "Patil", email: "tanmay.ashok.patil@gmail.com"}.freeze
+
+DEFAULT_CREDS = {client_id: "3aa48b03449f48bf5b3b", client_secret: "bbc8f069b405a3f2291fc3f8394df41306135b59"}.freeze
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 admin_users = User.where(role: "admin")
 if(admin_users.count==0)
-	admin = User.new(first_name: "Tanmay", last_name: "Patil", role: "admin")
-	e = EmailAddress.new(email: "tanmay.ashok.patil@gmail.com")
+	admin = User.new(first_name: ADMINISTRATOR[:first_name], last_name: ADMINISTRATOR[:last_name], role: "admin")
+	e = EmailAddress.new(email: ADMINISTRATOR[:email])
 	admin.email_addresses << e
 	admin.save
 end
 
-ApiConfiguration.create(client_id: "3aa48b03449f48bf5b3b", client_secret: "bbc8f069b405a3f2291fc3f8394df41306135b59")
+ApiConfiguration.create(client_id: DEFAULT_CREDS[:client_id], client_secret: DEFAULT_CREDS[:client_secret])
